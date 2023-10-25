eebc1957a62f2c26d38466de6da2112916c3808b
FinnanB <123640726+FinnanB@users.noreply.github.com>
2023-10-25 15:08:39 +0100

    enemy racers

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -11,25 +11,94 @@ public class EnemyController : MonoBehaviour
     public float maxSpeed = 20;
     public float turnRange;
 
-    public Transform[] turns = new  Transform[16];
+    //public Transform[] turns = new  Transform[16];
+    Transform[] turns;
+    GameObject controller;
     int dToward;
-    private float distance;
+    float distance;
+
+    float accelerationInput = 0;
+    float velocityVsUp = 0;
 
     private void Awake()
     {
+        controller = GameObject.Find("EnemyController");
+        turns = new Transform[controller.transform.childCount];
+        for(int i = 0; i < turns.Length; i++)
+        {
+            turns[i] = controller.transform.GetChild(i);
+        }
         rb = GetComponent<Rigidbody2D>();
     }
     void FixedUpdate()
     {
-        distance = Vector2.Distance(transform.position, turns[dToward].transform.position);
+        if (GameManager.instance.GetGameState() == GameStates.countDown)
+        {
+            return;
+        }
+        Turn();
         Vector2 direction = turns[dToward].transform.position - transform.position;
         direction.Normalize();
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(-direction.x, direction.y) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        ApplyEngineForce();
+        KillOrthogonalVelocity();
 
-        Vector2 engineForceVector = transform.up * 1 * accelerationFactor;
-
-        //rb.AddForce(engineForceVector, ForceMode2D.Force);
     }
 
+    void Turn()
+    {
+        distance = Vector2.Distance(transform.position, turns[dToward].transform.position);
+        if(distance > turnRange)
+        {
+            accelerationInput = 1;
+        }
+        else
+        {
+            accelerationInput = 0;
+        }
+        if(distance < turnRange)
+        {
+            dToward++;
+        }
+        if(dToward == turns.Length)
+        {
+            dToward = 0;
+        }
+    }
+    void ApplyEngineForce()
+    {
+        //calculate how much forward we are going in terms of the direction of our velocity
+        velocityVsUp = Vector2.Dot(transform.up, rb.velocity);
+
+        //limit so we cannot go faster than the max speed going forwards
+        if (velocityVsUp > maxSpeed && accelerationInput > 0)
+            return;
+
+        //limit so we cannot go faster than the 50% of max speed in reverse
+        if (velocityVsUp < -maxSpeed * 0.5f && accelerationInput < 0)
+            return;
+
+        //limit so we cannot go faster in any direction while accelerating
+        if (rb.velocity.sqrMagnitude > maxSpeed * maxSpeed && accelerationInput > 0)
+            return;
+
+        //apply drag if there is no accelerationInput so the car stops when the player lets go of the accelerator
+        if (accelerationInput == 0)
+            rb.drag = Mathf.Lerp(rb.drag, 3.0f, Time.fixedDeltaTime * 3);
+        else rb.drag = 0;
+
+        //create a force for the engine
+        Vector2 engineForceVector = transform.up * accelerationInput * accelerationFactor;
+
+        //apply force and pushes the car forward
+        rb.AddForce(engineForceVector, ForceMode2D.Force);
+    }
+
+    void KillOrthogonalVelocity()
+    {
+        Vector2 forwardVelocity = transform.up * Vector2.Dot(rb.velocity, transform.up);
+        Vector2 rightVelocity = transform.right * Vector2.Dot(rb.velocity, transform.right);
+        rb.velocity = forwardVelocity + rightVelocity * driftFactor;
+    }
 }
