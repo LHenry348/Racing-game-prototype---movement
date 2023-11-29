e1b07d19902e940a9c6eafbe7d3286ba96b42ba9
FinnanB <123640726+FinnanB@users.noreply.github.com>
2023-11-29 00:17:42 +0000

    pause menu

--- Assets/Scripts/inGameMenuUIHandler.cs
+++ Assets/Scripts/inGameMenuUIHandler.cs
@@ -2,16 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class inGameMenuUIHandler : MonoBehaviour
 {
     Canvas canvas;
+    public GameObject resumeButton;
+    public GameObject resetButton;
+    public Text heading;
 
     private void Awake()
     {
+        heading.text = "Race over!";
         canvas = GetComponent<Canvas>();
         canvas.enabled = false;
         GameManager.instance.OnGameStateChanged += OnGameStateChanged;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            heading.text = "Race paused!";
+            Time.timeScale = 0;
+            resetButton.SetActive(false);
+            resumeButton.SetActive(true);
+            canvas.enabled = true;
+            Debug.Log("Return key was pressed.");
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        resetButton.SetActive(true);
+        resumeButton.SetActive(false);
+        heading.text = "Race over!";
+        canvas.enabled = false;
     }
 
     public void OnRaceAgain()
